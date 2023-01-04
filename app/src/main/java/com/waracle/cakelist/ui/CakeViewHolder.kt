426b2eefdc02f1741eb6c1abd7ddafd3f7030604
gitton <gittoniclavose@gmail.com>
2023-01-04 13:46:17 +0000

    - fixes  - added black color to error message in case of dark mode

--- app/src/main/java/com/waracle/cakelist/ui/CakeViewHolder.kt
+++ app/src/main/java/com/waracle/cakelist/ui/CakeViewHolder.kt
@@ -14,15 +14,16 @@ class CakeViewHolder(private val cakeItemBinding: CakeItemBinding, private val l
 
     init {
         //Listener is attached to view holder itemview
-        cakeItemBinding.root.setOnClickListener(View.OnClickListener {
+        cakeItemBinding.root.setOnClickListener() {
             listener.onClick(adapterPosition)
-        })
+        }
     }
 
 
     fun bind(item : CakeItem)
     {
         cakeItemBinding.tvCakeName.text = item.title
+        //TODO : Could add a loading icon when fetching images and any other image if unable to fetch
         Picasso.get().load(item.image).into(cakeItemBinding.ivCakeImage)
     }
 }
